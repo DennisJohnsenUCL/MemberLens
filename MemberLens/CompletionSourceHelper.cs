6c0b6c1a30865d710594aa2ca0354bf76302d861
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2026-03-18 19:58:26 +0100

    Fixed unbound generic types not returning any members.
    Symbols for unbound generics return no members, you need the open generic to find members.

--- MemberLens/CompletionSourceHelper.cs
+++ MemberLens/CompletionSourceHelper.cs
@@ -96,6 +96,8 @@ namespace MemberLens
             }
             else return null;
 
+            if (sourceType.IsUnboundGenericType) sourceType = sourceType.OriginalDefinition;
+
             return sourceType;
         }
 
