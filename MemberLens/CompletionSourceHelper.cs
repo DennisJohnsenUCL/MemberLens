2777a16755409175021fb056756eb9d5c7234160
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2026-03-18 14:43:54 +0100

    Moved static helpers methods for completion source into their own static class.

---
+++ MemberLens/CompletionSourceHelper.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MemberLens.Attributes;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.VisualStudio.Text;
+
+namespace MemberLens
+{
+    internal class CompletionSourceHelper
+    {
+        internal static async Task<CompletionSymbolContext> GetSymbolContextAsync(SnapshotPoint triggerLocation, CancellationToken token = default)
+        {
+            var doc = triggerLocation.Snapshot.GetOpenDocumentInCurrentContextWithChanges();
+            if (doc == null) return null;
+
+            var syntaxTree = await doc.GetSyntaxTreeAsync(token);
+            if (syntaxTree == null) return null;
+
+            var root = await syntaxTree.GetRootAsync(token);
+
+            if (token.IsCancellationRequested) return null;
+
+            var position = triggerLocation.Position;
+            var tokenParent = root.FindToken(position).Parent;
+            if (tokenParent == null) return null;
+
+            var argumentListSyntax = tokenParent.FirstAncestorOrSelf<ArgumentListSyntax>();
+            if (argumentListSyntax == null) return null;
+
+            var expressionSyntax = argumentListSyntax.Parent;
+            if (expressionSyntax == null) return null;
+
+            var semanticModel = await doc.GetSemanticModelAsync(token);
+            if (semanticModel == null) return null;
+
+            if (token.IsCancellationRequested) return null;
+
+            var methodSymbolInfo = semanticModel.GetSymbolInfo(expressionSyntax, token);
+            int argumentIndex = argumentListSyntax.Arguments.GetSeparators().Count(separator => separator.SpanStart < position);
+
+            return new CompletionSymbolContext(methodSymbolInfo, argumentIndex, semanticModel);
+        }
+
+        internal static (IMethodSymbol MethodSymbol, AttributeData MemberAccessorAttribute)? GetSymbolAndAttribute(
+            SymbolInfo methodSymbolInfo, int argumentSymbolIndex)
+        {
+            var candidates = methodSymbolInfo.Symbol is IMethodSymbol directMatch
+                ? new[] { directMatch }
+                : methodSymbolInfo.CandidateSymbols.Cast<IMethodSymbol>();
+
+            foreach (var candidate in candidates)
+            {
+                if (argumentSymbolIndex >= candidate.Parameters.Length) continue;
+
+                var parameterSymbol = candidate.Parameters[argumentSymbolIndex];
+                if (parameterSymbol.Type.Name != "String") continue;
+
+                var memberAccessorAttribute = parameterSymbol.GetAttributes()
+                    .FirstOrDefault(ad => ad.AttributeClass?.Name == nameof(MemberAccessorAttribute));
+                if (memberAccessorAttribute == null) continue;
+
+                return (candidate, memberAccessorAttribute);
+            }
+
+            return null;
+        }
+
+        internal static INamedTypeSymbol GetSourceType(IMethodSymbol methodSymbol, ImmutableArray<TypedConstant> constructorArgs)
+        {
+            INamedTypeSymbol sourceType;
+            if (constructorArgs[1].Kind == TypedConstantKind.Type)
+            {
+                sourceType = (INamedTypeSymbol)constructorArgs[1].Value;
+            }
+            else if (constructorArgs.Length == 3)
+            {
+                var genericSources = (GenericSource)(int)constructorArgs[1].Value;
+                var genericIndex = (int)constructorArgs[2].Value;
+
+                if (genericSources == GenericSource.Method)
+                {
+                    if (genericIndex >= methodSymbol.TypeArguments.Length) return null;
+                    sourceType = (INamedTypeSymbol)methodSymbol.TypeArguments[genericIndex];
+                }
+                else if (genericSources == GenericSource.Class)
+                {
+                    var sourceClass = methodSymbol.ContainingType;
+                    if (genericIndex >= sourceClass.TypeArguments.Length) return null;
+                    sourceType = (INamedTypeSymbol)sourceClass.TypeArguments[genericIndex];
+                }
+                else return null;
+            }
+            else return null;
+
+            return sourceType;
+        }
+
+        internal static SnapshotSpan GetSnapshotSpan(ITextSnapshot snapshot, int position)
+        {
+            var start = position - 1;
+            while (true)
+            {
+                if (start <= 0) break;
+                var c = snapshot[start - 1];
+                if (!char.IsLetterOrDigit(c) && c != '_' && c != '"') break;
+                else
+                {
+                    start--;
+                }
+            }
+
+            var end = position;
+            while (true)
+            {
+                if (end >= snapshot.Length) break;
+                var c = snapshot[end];
+                if (!char.IsLetterOrDigit(c) && c != '_' && c != '"') break;
+                else
+                {
+                    end++;
+                }
+            }
+
+            return new SnapshotSpan(snapshot, start, end - start);
+        }
+    }
+}
--- MemberLens/MemberAccessorCompletionSource.cs
+++ MemberLens/MemberAccessorCompletionSource.cs
@@ -1,11 +1,8 @@
-﻿using System.Collections.Immutable;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MemberLens.Attributes;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion;
 using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion.Data;
@@ -19,17 +16,17 @@ namespace MemberLens
 
         public async Task<CompletionContext> GetCompletionContextAsync(IAsyncCompletionSession session, CompletionTrigger trigger, SnapshotPoint triggerLocation, SnapshotSpan applicableToSpan, CancellationToken token)
         {
-            var symCtx = await GetSymbolContextAsync(triggerLocation, token);
+            var symCtx = await CompletionSourceHelper.GetSymbolContextAsync(triggerLocation, token);
             if (symCtx == null) return Empty;
 
-            var symbolAndAttribute = GetSymbolAndAttribute(symCtx.MethodSymbolInfo, symCtx.ArgumentIndex);
+            var symbolAndAttribute = CompletionSourceHelper.GetSymbolAndAttribute(symCtx.MethodSymbolInfo, symCtx.ArgumentIndex);
             if (symbolAndAttribute == null) return Empty;
             var (methodSymbol, memberAccessorAttribute) = symbolAndAttribute.Value;
 
             var constructorArgs = memberAccessorAttribute.ConstructorArguments;
             if (constructorArgs.Length < 2) return Empty;
 
-            var sourceType = GetSourceType(methodSymbol, constructorArgs);
+            var sourceType = CompletionSourceHelper.GetSourceType(methodSymbol, constructorArgs);
             if (sourceType == null) return Empty;
 
             var accessorType = (AccessorType)(int)constructorArgs[0].Value;
@@ -66,128 +63,11 @@ namespace MemberLens
             var initial = snapshot[position - 1];
             if (char.IsLetterOrDigit(initial) || initial == '_' || initial == '"')
             {
-                var snapshotSpan = GetSnapshotSpan(snapshot, position);
+                var snapshotSpan = CompletionSourceHelper.GetSnapshotSpan(snapshot, position);
                 return new CompletionStartData(CompletionParticipation.ProvidesItems, snapshotSpan);
             }
 
             return new CompletionStartData(CompletionParticipation.ProvidesItems, new SnapshotSpan(snapshot, position, 0));
-        }
-
-        //TODO: Move this and below to other static class
-        private static async Task<CompletionSymbolContext> GetSymbolContextAsync(SnapshotPoint triggerLocation, CancellationToken token = default)
-        {
-            var doc = triggerLocation.Snapshot.GetOpenDocumentInCurrentContextWithChanges();
-            if (doc == null) return null;
-
-            var syntaxTree = await doc.GetSyntaxTreeAsync(token);
-            if (syntaxTree == null) return null;
-
-            var root = await syntaxTree.GetRootAsync(token);
-
-            if (token.IsCancellationRequested) return null;
-
-            var position = triggerLocation.Position;
-            var tokenParent = root.FindToken(position).Parent;
-            if (tokenParent == null) return null;
-
-            var argumentListSyntax = tokenParent.FirstAncestorOrSelf<ArgumentListSyntax>();
-            if (argumentListSyntax == null) return null;
-
-            var expressionSyntax = argumentListSyntax.Parent;
-            if (expressionSyntax == null) return null;
-
-            var semanticModel = await doc.GetSemanticModelAsync(token);
-            if (semanticModel == null) return null;
-
-            if (token.IsCancellationRequested) return null;
-
-            var methodSymbolInfo = semanticModel.GetSymbolInfo(expressionSyntax, token);
-            int argumentIndex = argumentListSyntax.Arguments.GetSeparators().Count(separator => separator.SpanStart < position);
-
-            return new CompletionSymbolContext(methodSymbolInfo, argumentIndex, semanticModel);
-        }
-
-        private static (IMethodSymbol MethodSymbol, AttributeData MemberAccessorAttribute)? GetSymbolAndAttribute(
-            SymbolInfo methodSymbolInfo, int argumentSymbolIndex)
-        {
-            var candidates = methodSymbolInfo.Symbol is IMethodSymbol directMatch
-                ? new[] { directMatch }
-                : methodSymbolInfo.CandidateSymbols.Cast<IMethodSymbol>();
-
-            foreach (var candidate in candidates)
-            {
-                if (argumentSymbolIndex >= candidate.Parameters.Length) continue;
-
-                var parameterSymbol = candidate.Parameters[argumentSymbolIndex];
-                if (parameterSymbol.Type.Name != "String") continue;
-
-                var memberAccessorAttribute = parameterSymbol.GetAttributes()
-                    .FirstOrDefault(ad => ad.AttributeClass?.Name == nameof(MemberAccessorAttribute));
-                if (memberAccessorAttribute == null) continue;
-
-                return (candidate, memberAccessorAttribute);
-            }
-
-            return null;
-        }
-
-        private static INamedTypeSymbol GetSourceType(IMethodSymbol methodSymbol, ImmutableArray<TypedConstant> constructorArgs)
-        {
-            INamedTypeSymbol sourceType;
-            if (constructorArgs[1].Kind == TypedConstantKind.Type)
-            {
-                sourceType = (INamedTypeSymbol)constructorArgs[1].Value;
-            }
-            else if (constructorArgs.Length == 3)
-            {
-                var genericSources = (GenericSource)(int)constructorArgs[1].Value;
-                var genericIndex = (int)constructorArgs[2].Value;
-
-                if (genericSources == GenericSource.Method)
-                {
-                    if (genericIndex >= methodSymbol.TypeArguments.Length) return null;
-                    sourceType = (INamedTypeSymbol)methodSymbol.TypeArguments[genericIndex];
-                }
-                else if (genericSources == GenericSource.Class)
-                {
-                    var sourceClass = methodSymbol.ContainingType;
-                    if (genericIndex >= sourceClass.TypeArguments.Length) return null;
-                    sourceType = (INamedTypeSymbol)sourceClass.TypeArguments[genericIndex];
-                }
-                else return null;
-            }
-            else return null;
-
-            return sourceType;
-        }
-
-        private static SnapshotSpan GetSnapshotSpan(ITextSnapshot snapshot, int position)
-        {
-            var start = position - 1;
-            while (true)
-            {
-                if (start <= 0) break;
-                var c = snapshot[start - 1];
-                if (!char.IsLetterOrDigit(c) && c != '_' && c != '"') break;
-                else
-                {
-                    start--;
-                }
-            }
-
-            var end = position;
-            while (true)
-            {
-                if (end >= snapshot.Length) break;
-                var c = snapshot[end];
-                if (!char.IsLetterOrDigit(c) && c != '_' && c != '"') break;
-                else
-                {
-                    end++;
-                }
-            }
-
-            return new SnapshotSpan(snapshot, start, end - start);
         }
     }
 }
